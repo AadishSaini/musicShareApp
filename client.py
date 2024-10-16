30cb4852594d32b13524fc4fc03dc2ac5937c2a9
unnamed_variable <63640902+AadishSaini@users.noreply.github.com>
2024-10-16 17:37:13 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,27 +1,5 @@
 from bin.initScreen import *
-import threading
-import socket
 from bin.handleClient import *
-
-def start_client(host, port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
-
-    # try:
-    #     while True:
-    #         message = input("Enter message: ")
-    #         if message.lower() == 'exit':
-    #             break
-    #         client.send(message.encode())
-    #         response = client.recv(1024)
-    #         print(f"[SERVER]: {response.decode()}")
-    # finally:
-    #     client.close()
-
-
-
-
-
 
 initS = initS()
 
@@ -31,8 +9,5 @@ ip, port= initS.get_ip_wanted()
 cObj = handleClient(ip, port)
 
 
-# while cObj.running:
-# server_recv_thread = threading.Thread(target=cObj.handleClient)
-# server_recv_thread.start()
+
 cObj.handleClient()
-# start_client(ip, port)
